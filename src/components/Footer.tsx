72ac813e5fd7e19b36cf1fad8deec3ed23209494
Ahamed, Afrose <AFROSE_AHAMED+comcast@comcast.com>
2026-03-02 11:55:21 +0530

    dsds

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,7 +26,7 @@ export default function Footer({ directorName = "Bala" }: FooterProps) {
             letterSpacing: "0.18em",
           }}
         >
-          © {new Date().getFullYear()} House if eleven11
+          © {new Date().getFullYear()} House of eleven11
         </Link>
 
         {/* Social links */}
